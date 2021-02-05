57f09782a79bc1375cbbc536c95265f6f0bad92b
Millard Arnold V <clear.the.spam@gmail.com>
2021-02-04 17:18:28 -0900

    Test to check if empty board is created

--- js/tests/board.test.js
+++ js/tests/board.test.js
@@ -1,4 +1,8 @@
-const sum = require('../board');
+/*const sum = require('../board');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
-});
+});*/
+const initBoard = require('../board');
+test('Creates an empty board', () => {
+    expect(initBoard()).toStrictEqual([[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]])
+})
