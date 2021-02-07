93891082271481b756b6de27702325032b36f1e2
imthegngrbrdmn <clear.the.spam@gmail.com>
2021-02-06 16:07:04 -0900

    Removed Board and added some event listeners

--- js/board.js
+++
@@ -1,4 +0,0 @@
-function initBoard () {
-  const board = new Array(8).fill(0).map(() => new Array(8).fill(0))
-  return board
-}
--- js/init.js
+++ js/init.js
@@ -1,13 +1,15 @@
 
 const board = document.getElementsByClassName('board')[0]
 for (let i = 0; i < 64; i++) {
-  board.appendChild(document.createElement('div'))
+  const currentSquare = board.appendChild(document.createElement('div'))
+  currentSquare.addEventListener('click', () => console.log(i))
 }
 const ships = initShips()
 const shipContainer = document.getElementsByClassName('ship-container')[0]
 for (ship of ships) {
   const currentShip = shipContainer.appendChild(document.createElement('div'))
   currentShip.className = 'ship'
+  currentShip.addEventListener('click', () => currentShip.style.border = '5px solid black')
   for (square of ship.body) {
     const currentSquare = currentShip.appendChild(document.createElement('div'))
     currentSquare.className = 'ship-body'
