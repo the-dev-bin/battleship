caef2ee77493de89262bb15526a6b58ce583a7b4
Tyler Chase <tylercchase@gmail.com>
2021-02-06 15:15:32 -0900

    Add ships to html

--- js/init.js
+++ js/init.js
@@ -3,3 +3,13 @@ let board = document.getElementsByClassName("board")[0];
 for(let i = 0; i < 64; i++){
     board.appendChild(document.createElement('div'));
 }
+let ships = initShips();
+let shipContainer = document.getElementsByClassName("ship-container")[0];
+for(ship of ships){
+    let currentShip = shipContainer.appendChild(document.createElement('div'));
+    currentShip.className = 'ship'
+    for(square of ship.body){
+        let currentSquare = currentShip.appendChild(document.createElement('div'));
+        currentSquare.className = 'ship-body'
+    }
+}
